340fbb908ab0961ff61f2963c4764a5a041e25b9
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-05-10 22:47:15 +0545

    feat: socket io server implement

--- Real_Time_chat_application/backend/server.js
+++ Real_Time_chat_application/backend/server.js
@@ -7,6 +7,7 @@ import cors from 'cors'
 import { errorHandler, notFound } from "./src/helper/errorHandler.js";
 import chatRoutes from "./src/router/chatRoutes.js";
 import messageRoutes from "./src/router/messageRoutes.js";
+import { Server } from 'socket.io'
 const app = express()
 
 connectDb()
@@ -19,9 +20,39 @@ app.use('/api/chats', chatRoutes)
 app.use('/api/messages', messageRoutes)
 app.use(notFound)
 app.use(errorHandler)
-app.use(cors({
-    origin: '*'
-}));
-app.listen(PORT, () => {
+// app.use(cors({
+//     origin: '*',
+//     credentials: true
+// }))
+const server = app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
+})
+const io = new Server(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+})
+io.on('connection', (socket) => {
+    console.log('Connected to socket')
+    socket.on('setup', (userData) => {
+        socket.join(userData._id)
+        console.log(userData._id)
+        socket.emit('connected')
+    })
+    socket.on('join chat', (room) => {
+        socket.join(room)
+        console.log('user jooin room' + room)
+    })
+    socket.on('new message', (newMessageReceived) => {
+        let chat = newMessageReceived.chat
+
+        if (!chat.users) return console.log('chat.users not defined')
+
+        chat.users.forEach(user => {
+            if (user._id === newMessageReceived.sender._id) return
+            socket.in(user._id).emit("message recieved", newMessageReceived)
+        })
+
+    })
 })
