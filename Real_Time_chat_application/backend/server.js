7d201b7a3512ecb0089b4de2e89b64c5d6e1ee85
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-05-11 13:56:05 +0545

    Feat: Modified Socket

--- Real_Time_chat_application/backend/server.js
+++ Real_Time_chat_application/backend/server.js
@@ -44,6 +44,10 @@ io.on('connection', (socket) => {
         socket.join(room)
         console.log('user jooin room' + room)
     })
+
+    socket.on('typing', (room) => socket.in(room).emit('typing'))
+    socket.on('stop typing', (room) => socket.in(room).emit('stop typing'))
+
     socket.on('new message', (newMessageReceived) => {
         let chat = newMessageReceived.chat
 
@@ -53,6 +57,9 @@ io.on('connection', (socket) => {
             if (user._id === newMessageReceived.sender._id) return
             socket.in(user._id).emit("message recieved", newMessageReceived)
         })
-
+    })
+    socket.off("setup", () => {
+        console.log('User Disconnected')
+        socket.leave(userData._id)
     })
 })
